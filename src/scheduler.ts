08a5fe48ba1fe932052c13ee66112a08de5c2f02
Vlad Malinin <vladmalinin1999@gmail.com>
2025-11-23 18:13:27 +0200

    fix: fix time

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -3,13 +3,25 @@ import { postRandomChallenge } from "./postChallenge.js";
 import { markAsFinished } from "./updateMessage.js";
 
 export function startScheduler() {
-  cron.schedule("10 18 * * *", () => {
-    console.log("Running test CodeWars challenge at 17:10...");
-    postRandomChallenge();
-  });
+  cron.schedule(
+    "17 18 * * *",
+    async () => {
+      console.log("Running test CodeWars challenge at 18:10 Europe/Kyiv");
+      await postRandomChallenge();
+    },
+    {
+      timezone: "Europe/Kiev", // або "Europe/Kyiv" залежно від версії tz
+    }
+  );
 
-  cron.schedule("11 18 * * *", () => {
-    console.log("Marking as finished…");
-    markAsFinished();
-  });
+  cron.schedule(
+    "18 18 * * *",
+    async () => {
+      console.log("Marking as finished at 18:11 Europe/Kyiv");
+      await markAsFinished();
+    },
+    {
+      timezone: "Europe/Kiev",
+    }
+  );
 }
